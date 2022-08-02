80173b919aad6d9bb6310d73b9f3719083c91757
syed <user.email>
2022-08-02 14:43:30 +0100

    formatted plus added image alt

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import EpisodeApp from "./components/TVEpisodes";
-import episodes from "./episodes.json"
-import "./App.css"
-
+import episodes from "./episodes.json";
+import "./App.css";
 
 function App(): JSX.Element {
-  return(
-    <>      
-      <EpisodeApp {...episodes[0]}/>
+  return (
+    <>
+      <EpisodeApp {...episodes[0]} />
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/TVEpisodes.tsx
+++ src/components/TVEpisodes.tsx
@@ -1,41 +1,38 @@
 interface IEpisode {
-    "id": number,
-    "url": string
-    "name": string
-    "season": number
-    "number": number
-    "type": string
-    "airdate": string
-    "airtime": string
-    "airstamp": string
-    "runtime": number
-    "rating": {
-        "average": number
-    },
-    "image": { 
-        "medium": string
-        "original": string
-    },
-    "summary": string
-    "_links": {
-        "self": {
-            "href": string
-        }
-    }
+  id: number;
+  url: string;
+  name: string;
+  season: number;
+  number: number;
+  type: string;
+  airdate: string;
+  airtime: string;
+  airstamp: string;
+  runtime: number;
+  rating: {
+    average: number;
+  };
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  _links: {
+    self: {
+      href: string;
+    };
+  };
 }
 
-function EpisodeApp(props: IEpisode):JSX.Element {
-    return(
-        <>
-            <h2>{props.name}</h2> 
-            <img src={props.image.medium}></img> 
-            <h3>episode season + number</h3>
-            <p> description</p>          
-        </>
-    )
+function EpisodeApp(props: IEpisode): JSX.Element {
+  return (
+    <>
+      <h2>{props.name}</h2>
+      <img src={props.image.medium} alt="episodeImage"></img>
+      <h3>episode season + number</h3>
+      <p> description</p>
+    </>
+  );
 }
 
-
-export default EpisodeApp
-
-
+export default EpisodeApp;
