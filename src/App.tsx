e1ca4446779e523b077e8ee638069d22f1e15982
syed <user.email>
2022-08-03 15:37:59 +0100

    added footer with source

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import EpisodeApp from "./components/TVEpisodes";
 import episodes from "./episodes.json";
 import "./App.css";
+import FooterApp from "./AppFooter";
 
 function App(): JSX.Element {
   return (
-    <div className="main-content">
-      {episodes.map((episode) => (
-        <EpisodeApp key={episode.name} {...episode} />
-      ))}
+    <div>
+      <div className="main-content">
+        {episodes.map((episode) => (
+          <EpisodeApp key={episode.name} {...episode} />
+        ))}
+      </div>
+      <FooterApp />
     </div>
   );
 }
--- src/utils/createEpisodeCode.test.ts
+++ src/utils/createEpisodeCode.test.ts
@@ -1,9 +1,8 @@
 import { createEpisodeCode } from "./createEpisodeCode";
 
 test("returns code with format S02E07 given season and episode number", () => {
-    expect(createEpisodeCode(1,1)).toBe("S01E01");
-    expect(createEpisodeCode(1,2)).toBe("S01E02");
-    expect(createEpisodeCode(1,12)).toBe("S01E12");
-    expect(createEpisodeCode(12,12)).toBe("S12E12");
-  });
-  
+  expect(createEpisodeCode(1, 1)).toBe("S01E01");
+  expect(createEpisodeCode(1, 2)).toBe("S01E02");
+  expect(createEpisodeCode(1, 12)).toBe("S01E12");
+  expect(createEpisodeCode(12, 12)).toBe("S12E12");
+});
