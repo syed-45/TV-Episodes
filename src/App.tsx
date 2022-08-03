18eaf87402507323539bc1f8ed078143a0e80f84
syed <user.email>
2022-08-03 16:05:33 +0100

    added header component and livesearch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import EpisodeApp from "./components/TVEpisodes";
 import episodes from "./episodes.json";
 import "./App.css";
-import FooterApp from "./AppFooter";
+import FooterApp from "./components/AppFooter";
+import HeaderApp from "./components/AppHeader";
 
 function App(): JSX.Element {
   return (
     <div>
+      <HeaderApp/>
       <div className="main-content">
         {episodes.map((episode) => (
           <EpisodeApp key={episode.name} {...episode} />
